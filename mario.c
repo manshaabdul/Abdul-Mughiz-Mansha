0948cd60545b840f4b8f7f159227389719180301
manshaabdul <57458482+manshaabdul@users.noreply.github.com>
2019-11-29 13:11:12 +0500

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,35 @@
+[12:46 PM, 11/29/2019] Abdul Mughiz Mansha: #include <cs50.h>
+#include <stdio.h>
+
+void build_pyramid(int height);
+
+int main(void)
+{
+    int height = -1;
+    do
+    {
+        printf("Height: ");
+        height = get_int();
+    }
+    while (height < 0 || height > 23);
+    build_pyramid(height);
+}
+
+void build_pyramid(int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - 1 - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i + 2; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
+submit50 cs50/problems/2019/x/mario/less
